d5dba33c88937bf777ba570b6879468911470db2
User <taranovegor91@gmail.com>
2021-12-21 10:22:47 +0300

    исправил лишние пробелы в жизни и творчестве

--- Unit14/app/src/main/java/teach/meskills/unit14/FormatLetters.kt
+++ Unit14/app/src/main/java/teach/meskills/unit14/FormatLetters.kt
@@ -36,61 +36,25 @@ class FormatLetters : AppCompatActivity() {
                 }
                 formattedText += array[i]
             }
-
-
-            // inputText.setText(formattedText) закоментировал
-
-            var helpStr =
-                ""                                 // создадим вспомогательную строковую переменную в которую будем по буквам собирать слова
-            var str =
-                ""                                  // создадим вспомогательную строковую переменную в которую будем по буквам собирать слова и еще менять цифры на слова
-            val ch =
-                (formattedText + " ").toCharArray()             // создадим чаровый массив из уже отформатированной пользовательской строки
-
-            for (i in 0 until ch.size) {
-                if (ch[i] == '1' && ch[i + 1] == '0') {
-                    str = str + "десять "
-                } else if (numberMap.containsKey(ch[i].toString())) {
-                    str = str + numberMap.getValue(ch[i].toString())
-                } else {
-                    if (ch[i] == '0' && ch[i - 1] == '1') {
-                        continue
-                    } else {
-                        str = str + ch[i]
-                    }
-                }
-            }
-            val numbersAsWordsCharArray =
-                str.toCharArray()           // новый чаровый мвссив с поменяными цифрами на слова
-            val words =
-                mutableListOf<String>()                // создадим лист для хранения полученных отдельных слов
-            for (i in 0 until numbersAsWordsCharArray.size) {       // пробежимся фором по чаровому массиву по буквам собирая слова
-                helpStr = helpStr + numbersAsWordsCharArray[i]
-                if (numbersAsWordsCharArray[i] == ' ' || i == numbersAsWordsCharArray.size - 1) {         // натыкаясь на пробел или на конец строки ( чарового массива в данном случае ), кладем слово в лист
-                    // предварительно обрежим пробел с конца слова , чтоб не мешал
-                    words.add(helpStr.trim())                                     //теперь кладем в лист
-                    helpStr =
-                        ""                                        // и обнуляем строку , чтоб после пробела собиралось следующее слово, а не наклеивалось на предыдущее
-                }
-            }
-            inputText.setText("")                               // теперь обнулим эдитТекст, чтоб вдальнейшем при использовании inputText.append(it+" ") текст не склеивался с предыдущим
-
-            words.sortWith(                                          // отсортируем по возрастанию, спасибо котлин
-                Comparator { cat1, cat2 ->
-                    cat1.length - cat2.length
-                }
-
-            )
-
-            var finaleS=""
-// бежим по отсортированному массиву форычем и добавляем по слову в конечную строку за каждую итерацию
-            words.forEach {
-
-                finaleS= finaleS+it+" "
-
+        // inputText.setText(formattedText) закоментировал
+        var string=formattedText
+            string.replace("10", numberMap.getValue("10"),true)
+                .also { string = it }
+            string.forEach {
+                if (numberMap.containsKey(it.toString()))
+                    string.replace(it.toString(), numberMap.getValue(it.toString()))
+                        .also { string = it }
             }
 
-            inputText.setText(finaleS.trim())
+
+            val sl = string.split(" ")
+
+            var stringList = sl.sortedWith(Comparator { word1, word2 ->
+                word1.length - word2.length
+
+            })
+
+            inputText.setText( stringList.joinToString(" "))
 
         }
 
